4dc8d4e3db66668f636af88f76c51c3e26f134d5
Atharv Rokde <152302584+Atharvrokde01@users.noreply.github.com>
2024-05-29 17:15:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-const scrollUp = document.querySelector("#scroll-up");
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
 
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
-const checkbox = document.querySelector("#checkbox");
-
-checkbox.addEventListener("change", () => {
-  // Toggle website theme
-  document.body.classList.toggle("dark");
-});
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("ul");
-
-function openMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
-const navLink = document.querySelectorAll("#nav-link");
-
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+window.onscroll = function () {
+    var top = window.scrollY;
+    if (top >= 100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
 }
